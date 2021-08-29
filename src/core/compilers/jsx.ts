81bdbb040bb450f9adc20843da50ad792972506c
Anthony Fu <anthonyfu117@hotmail.com>
2021-08-29 14:09:40 +0800

    fix: require `@svgr/core` dynamically

--- src/core/compilers/jsx.ts
+++ src/core/compilers/jsx.ts
@@ -1,8 +1,8 @@
-// @ts-expect-error
-import svgr from '@svgr/core'
 import { Options } from '../../types'
 
 export async function JSXCompiler(svg: string, collection: string, icon: string, style: Options['jsx'] = 'react') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const svgr = require('@svgr/core')
   let res = await svgr(svg, { icon: true }, { componentName: camel(`${collection}-${icon}`) })
   // svgr does not provide an option to support preact (WHY?),
   // we manually remove the react import for preact
