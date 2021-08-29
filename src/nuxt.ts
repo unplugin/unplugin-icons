6bf333570cf7d8b9252d438f4e54a37c9962afeb
Anthony Fu <anthonyfu117@hotmail.com>
2021-08-29 12:58:03 +0800

    fix: nuxt module pass options

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,15 +1,16 @@
+import { Options } from './types'
 import unplugin from '.'
 
-export default function(this: any) {
+export default function(this: any, options: Options) {
   // install webpack plugin
   this.extendBuild((config: any) => {
     config.plugins = config.plugins || []
-    config.plugins.unshift(unplugin.webpack())
+    config.plugins.unshift(unplugin.webpack(options))
   })
 
   // install vite plugin
   this.nuxt.hook('vite:extend', async(vite: any) => {
     vite.config.plugins = vite.config.plugins || []
-    vite.config.plugins.push(unplugin.vite())
+    vite.config.plugins.push(unplugin.vite(options))
   })
 }
