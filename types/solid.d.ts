6e41fde96cdf1faff4b435edb06a951fb2e0cdd4
Jorge Godoy <godoy.jf@gmail.com>
2021-09-01 20:33:35 -0400

    fix: types for solid (#44)

--- types/solid.d.ts
+++ types/solid.d.ts
@@ -1,11 +1,11 @@
 /* eslint-disable import/named */
 declare module 'virtual:icons/*' {
-  import { JSX } from 'solid-js'
-  const component: (props: JSX.StylableSVGAttributes) => JSX.Element
+  import { JSX, ComponentProps } from 'solid-js'
+  const component: (props: ComponentProps<'svg'>) => JSX.Element
   export default component
 }
 declare module '~icons/*' {
-  import { JSX } from 'solid-js'
-  const component: (props: JSX.StylableSVGAttributes) => JSX.Element
+  import { JSX, ComponentProps } from 'solid-js'
+  const component: (props: ComponentProps<'svg'>) => JSX.Element
   export default component
 }
