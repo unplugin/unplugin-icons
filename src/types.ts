b7628c5ff521136f1158ca6d0cb0e1454aa04ac2
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-09-12 00:55:20 +0200

    chore: only one svelte compiler

--- src/types.ts
+++ src/types.ts
@@ -25,7 +25,7 @@ export interface Options {
    *
    * @default (detect automatically, fallback to 'vue3')
    */
-  compiler?: 'vue2' | 'vue3' | 'jsx' | 'solid' | 'svelte' | 'svelte-kit'
+  compiler?: 'vue2' | 'vue3' | 'jsx' | 'solid' | 'svelte'
 
   /**
    * JSX style, works only when compiler set to `jsx`
@@ -33,32 +33,6 @@ export interface Options {
    * @default (detect automatically, fallback to 'react')
    */
   jsx?: 'react' | 'preact'
-
-  /**
-   * Svelte options, works only when compiler set to `svelte`.
-   *
-   * @see https://svelte.dev/docs#svelte_compile
-   */
-  svelte?: {
-    /**
-     * Module format
-     *
-     * @default 'esm'
-     */
-    format?: 'esm' | 'cjs'
-    /**
-     * Legacy mode?
-     *
-     * @default false
-     */
-    legacy?: boolean
-    /**
-     * Styles will be included in the JavaScript class and injected at runtime?
-     *
-     * @default true
-     */
-    css?: true
-  }
 }
 
 export type ResolvedOptions = Required<Options>
