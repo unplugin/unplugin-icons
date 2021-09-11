ed6e8567a5c11439eadf326d7208ca50cbb1ab1e
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-09-12 00:55:35 +0200

    chore: only one svelte compiler impl

--- src/core/compilers/svelte.ts
+++ src/core/compilers/svelte.ts
@@ -1,18 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import { Options } from '../../types'
-
-export async function SvelteCompiler(svg: string, collection: string, icon: string, options: Options['svelte'] = {}) {
-  const { compile } = await import('svelte/compiler')
-
-  const { js } = compile(svg, {
-    filename: `${collection}-${icon}.svelte`,
-    format: options.format || 'esm',
-    legacy: typeof options.legacy !== 'undefined' ? options.legacy : false,
-    generate: process.env.SSR ? 'ssr' : 'dom',
-    hydratable: true,
-    css: typeof options.css !== 'undefined' ? options.css : true,
-    namespace: 'svg',
-  })
-
-  return js.code
+// will be resolved by `vite-plugin-svelte`
+export async function SvelteCompiler(svg: string) {
+  return svg
 }
--- src/core/compilers/svelteKit.ts
+++
@@ -1,4 +0,0 @@
-// will be resolved by `vite-plugin-svelte`
-export async function SvelteKitCompiler(svg: string) {
-  return svg
-}
