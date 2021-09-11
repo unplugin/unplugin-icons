cf7ac141d51e6e8a770c7f76da366d17a503a8aa
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-09-11 14:54:43 +0200

    chore: add another collection icon

--- examples/vite-svelte/src/App.svelte
+++ examples/vite-svelte/src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import MdiStore24Hour from 'virtual:icons/mdi/store-24-hour'
     import MdiAlarmOff from 'virtual:icons/mdi/alarm-off'
+    import IconParkAbnormal from 'virtual:icons/icon-park/abnormal'
 </script>
 
 <main>
   <MdiStore24Hour />
   <MdiAlarmOff />
+  <IconParkAbnormal />
 </main>
 
