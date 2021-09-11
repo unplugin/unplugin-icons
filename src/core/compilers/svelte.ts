ae2786c3fecc7b6acd771b45d3f0ed884c769acb
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-09-11 23:39:04 +0200

    chore: update svelte compiler for ssr and always hydratable

--- src/core/compilers/svelte.ts
+++ src/core/compilers/svelte.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-
 import { Options } from '../../types'
 
 export async function SvelteCompiler(svg: string, collection: string, icon: string, options: Options['svelte'] = {}) {
-  const svelte = require('svelte/compiler')
+  const { compile } = await import('svelte/compiler')
 
-  const { js } = svelte.compile(svg, {
+  const { js } = compile(svg, {
     filename: `${collection}-${icon}.svelte`,
     format: options.format || 'esm',
     legacy: typeof options.legacy !== 'undefined' ? options.legacy : false,
-    generate: options.generate || 'dom',
+    generate: process.env.SSR ? 'ssr' : 'dom',
+    hydratable: true,
     css: typeof options.css !== 'undefined' ? options.css : true,
     namespace: 'svg',
   })
