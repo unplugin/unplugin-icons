7c378d697dc8932c018e64236735f8732a3f346f
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-09-11 14:04:47 +0200

    chore: add vite svelte example

---
+++ examples/vite-svelte/src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import MdiStore24Hour from 'virtual:icons/mdi/store-24-hour'
+    import MdiAlarmOff from 'virtual:icons/mdi/alarm-off'
+</script>
+
+<main>
+  <MdiStore24Hour />
+  <MdiAlarmOff />
+</main>
+
