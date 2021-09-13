22bafa3a10a034aa3834fc14b0533daae916e6ab
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-09-13 14:06:36 +0200

    chore: force plugin with `enforce: 'true'

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,17 @@ import { Options } from './types'
 const unplugin = createUnplugin<Options>((options = {}) => {
   const resolved = resolveOptions(options)
 
-  const svelte = options.compiler === 'svelte'
-
-  const enforce = svelte ? 'pre' : undefined
-
   return {
     name: 'unplugin-icons',
-    enforce,
+    enforce: 'pre',
     resolveId(id) {
       if (isIconPath(id)) {
         const res = normalizeIconPath(id)
           .replace(/\.\w+$/i, '')
           .replace(/^\//, '')
-        const ext = options.compiler === 'jsx' ? '.jsx' : (svelte ? '.svelte' : '')
+        const ext = options.compiler === 'jsx'
+          ? '.jsx'
+          : (options.compiler === 'svelte' ? '.svelte' : '')
         return res + ext
       }
       return null
