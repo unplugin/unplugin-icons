3632068c28145e56dd92b73b6ad786519f857735
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-09-13 14:53:43 +0200

    fix: add `import/no-duplicates` on preact, react, solid and vue3 types

--- types/solid.d.ts
+++ types/solid.d.ts
@@ -1,10 +1,12 @@
 /* eslint-disable import/named */
 declare module 'virtual:icons/*' {
+  // eslint-disable-next-line import/no-duplicates
   import { JSX, ComponentProps } from 'solid-js'
   const component: (props: ComponentProps<'svg'>) => JSX.Element
   export default component
 }
 declare module '~icons/*' {
+  // eslint-disable-next-line import/no-duplicates
   import { JSX, ComponentProps } from 'solid-js'
   const component: (props: ComponentProps<'svg'>) => JSX.Element
   export default component
--- types/vue3.d.ts
+++ types/vue3.d.ts
@@ -1,10 +1,12 @@
 /* eslint-disable import/named */
 declare module 'virtual:icons/*' {
+  // eslint-disable-next-line import/no-duplicates
   import { FunctionalComponent, SVGAttributes } from 'vue'
   const component: FunctionalComponent<SVGAttributes>
   export default component
 }
 declare module '~icons/*' {
+  // eslint-disable-next-line import/no-duplicates
   import { FunctionalComponent, SVGAttributes } from 'vue'
   const component: FunctionalComponent<SVGAttributes>
   export default component
