e0f681364ef4facf8ed7b84111d3240d97939d34
userquin <joaquin.sanchez@fi2net.es>
2021-09-15 23:27:29 +0200

    feat: add svelte compiler for props support (#60)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- src/core/compilers/index.ts
+++ src/core/compilers/index.ts
@@ -3,6 +3,7 @@ import { JSXCompiler } from './jsx'
 import { NoneCompiler } from './none'
 import { RawCompiler } from './raw'
 import { SolidCompiler } from './solid'
+import { SvelteCompiler } from './svelte'
 import { Compiler } from './types'
 import { Vue2Compiler } from './vue2'
 import { Vue3Compiler } from './vue3'
@@ -12,7 +13,7 @@ export const compilers: Record<ResolvedOptions['compiler'], Compiler> = {
   'vue2': Vue2Compiler,
   'vue3': Vue3Compiler,
   'solid': SolidCompiler,
-  'svelte': NoneCompiler,
+  'svelte': SvelteCompiler,
   'jsx': JSXCompiler,
   'none': NoneCompiler,
   'raw': RawCompiler,
---
+++ src/core/compilers/svelte.ts
@@ -0,0 +1,19 @@
+import { Compiler } from './types'
+
+export const SvelteCompiler = <Compiler>((svg: string) => {
+  const openTagEnd = svg.indexOf('>', svg.indexOf('<svg '))
+  const closeTagStart = svg.lastIndexOf('</svg')
+  const openTag = `${svg.slice(0, openTagEnd)} {...$$props}>`
+  const content = `{@html \`${escapeSvelte(svg.slice(openTagEnd, closeTagStart))}\`}`
+  const closeTag = svg.slice(closeTagStart)
+  return `${openTag}${content}${closeTag}`
+})
+
+// escape curlies, backtick, \t, \r, \n to avoid breaking output of {@html `here`} in .svelte
+export function escapeSvelte(str: string): string {
+  return str
+    .replace(/{/g, '&#123;')
+    .replace(/}/g, '&#125;')
+    .replace(/`/g, '&#96;')
+    .replace(/\\([trn])/g, ' ')
+}
