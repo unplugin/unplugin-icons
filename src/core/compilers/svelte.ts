cbc7df20307d1c212899e0d1adedd04658072df9
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-09-15 17:54:52 +0200

    chore: find svg close tag without `>`

--- src/core/compilers/svelte.ts
+++ src/core/compilers/svelte.ts
@@ -3,7 +3,7 @@ import { Compiler } from './types'
 
 export const SvelteCompiler = <Compiler>((svg: string) => {
   const openTagEnd = svg.indexOf('>', svg.indexOf('<svg '))
-  const closeTagStart = svg.lastIndexOf('</svg>')
+  const closeTagStart = svg.lastIndexOf('</svg')
   const openTag = `${svg.slice(0, openTagEnd)} {...$$props}>`
   const content = `{@html \`${escapeSvelte(svg.slice(openTagEnd, closeTagStart))}\`}`
   const closeTag = svg.slice(closeTagStart)
