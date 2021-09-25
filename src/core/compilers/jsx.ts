5c3799e166ff3616e60693a83ec60d28ca7e47c6
Anthony Fu <anthonyfu117@hotmail.com>
2021-09-25 11:37:44 +0800

    fix(jsx): respect scale, close #68

--- src/core/compilers/jsx.ts
+++ src/core/compilers/jsx.ts
@@ -4,10 +4,10 @@ import { Compiler } from './types'
 export const JSXCompiler = <Compiler>(async(svg, collection, icon, options) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const svgr = require('@svgr/core').default
-  let res = await svgr(svg, { icon: true }, { componentName: camelize(`${collection}-${icon}`) })
+  let res = await svgr(svg, {}, { componentName: camelize(`${collection}-${icon}`) })
   // svgr does not provide an option to support preact (WHY?),
   // we manually remove the react import for preact
-  if (options.jsx === 'preact')
+  if (options.jsx !== 'react')
     res = res.replace('import * as React from "react";', '')
   return res
 })
