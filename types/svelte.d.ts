53af1dcfcf7d3340f996374d77221b0099c80e8f
kafai <lamkafai1997@gmail.com>
2021-09-29 00:34:02 +0800

    fix svelte icon component typing

--- types/svelte.d.ts
+++ types/svelte.d.ts
@@ -1,11 +1,7 @@
-declare module 'virtual:icons/*' {
-  import { SvelteComponentDev } from 'svelte/internal'
-  const component: SvelteComponentDev
-  export default component
+declare module "virtual:icons/*" {
+  export { SvelteComponentDev as default } from "svelte/internal";
 }
 
-declare module '~icons/*' {
-  import { SvelteComponentDev } from 'svelte/internal'
-  const component: SvelteComponentDev
-  export default component
+declare module "~icons/*" {
+  export { SvelteComponentDev as default } from "svelte/internal";
 }
