0908f60074053e231eafba4fe02a4791b2bcc333
Anthony Fu <anthonyfu117@hotmail.com>
2021-10-05 13:52:47 +0800

    fix: duplicate svg id

--- src/core/compilers/vue3.ts
+++ src/core/compilers/vue3.ts
@@ -1,16 +1,19 @@
+import { handleSVGId } from '../svgId'
 import { Compiler } from './types'
 
 export const Vue3Compiler = <Compiler>(async(svg: string, collection: string, icon: string) => {
   const { compileTemplate } = await import('@vue/compiler-sfc')
 
+  const { injectScripts, svg: handled } = handleSVGId(svg)
+
   let { code } = compileTemplate({
-    source: svg,
+    source: handled,
     id: `${collection}:${icon}`,
     filename: `${collection}-${icon}.vue`,
   })
 
   code = code.replace(/^export /g, '')
-  code += '\n\nexport default { name: ' + `'${collection}-${icon}',` + ' render }'
+  code += `\n\nexport default { name: '${collection}-${icon}', render${ injectScripts ? `, data() {${injectScripts};return { idMap }}` : ''} }`
   code += '\n/* vite-plugin-components disabled */'
 
   return code
---
+++ src/core/svgId.ts
@@ -0,0 +1,27 @@
+const randIdFn = 'const __randId = () => Math.random().toString(36).substr(2, 10);'
+
+export function handleSVGId(svg: string) {
+  const hasID = /="url\(#/.test(svg)
+  const idMap: Record<string, string> = {}
+  let injectScripts = ''
+
+  if (hasID) {
+    svg = svg
+      .replace(/\b([\w-]+?)="url\(#(.+?)\)"/g, (_, s, id) => {
+        idMap[id] = `'${id}':'uicons-'+__randId()`
+        return `:${s}="'url(#'+idMap['${id}']+')'"`
+      })
+      .replace(/\bid="(.+?)"/g, (full, id) => {
+        if (idMap[id])
+          return `:id="idMap['${id}']"`
+        return full
+      })
+      injectScripts = randIdFn + `const idMap = {${Object.values(idMap).join(',')}};`
+  }
+
+  return {
+    hasID,
+    svg,
+    injectScripts,
+  }
+}
