21a35fa39151bc8f27301895dab5975f9b32da15
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-10-10 00:27:59 +0200

    chore: simplify css compiler

--- src/core/compilers/css.ts
+++ src/core/compilers/css.ts
@@ -7,23 +7,19 @@ export const CssCompiler = <Compiler>((
   icon: string,
   options: ResolvedOptions,
 ) => {
-  // we need to add the svg xml namespace
-  let inlineSvg = svg.includes('http://www.w3.org/2000/svg')
-    ? svg.replace(/"/g, '\'')
-    : svg.replace(/"/g, '\'').replace('<svg ', '<svg xmlns=\'http://www.w3.org/2000/svg\' ')
+  let inlineSvg = svg.replace(/"/g, '\'')
 
-  // we can use svg or base64 encoding
-  inlineSvg = options.css.type === 'base64'
-    ? `base64,${Buffer.from(inlineSvg, 'utf-8').toString('base64')}`
-    : `utf8,${inlineSvg}`
+  // we need to add the svg xml namespace
+  if (!inlineSvg.includes('http://www.w3.org/2000/svg'))
+    inlineSvg = inlineSvg.replace('<svg ', '<svg xmlns=\'http://www.w3.org/2000/svg\' ')
 
   const scale = options?.css.scale ?? 1.2
 
   return `.${collection}-${icon} {
-background-image: url("data:image/svg+xml;${inlineSvg}");
+background-image: url("data:image/svg+xml;base64,${Buffer.from(inlineSvg, 'utf-8').toString('base64')}");
+background-size: ${scale}em ${scale}em;
 height: ${scale}em;
 width: ${scale}em;
-background-size: ${scale}em ${scale}em;
 display: inline-block;
 ${options.css.defaultStyles || ''}
 }
