3d31424961e8292f9991a0c1abc1e336092a5e6f
Ian VanSchooten <ian.vanschooten@gmail.com>
2021-10-12 18:50:29 -0400

    fix: get `default` method from @svgr/core (fixes #96)

--- src/core/compilers/jsx.ts
+++ src/core/compilers/jsx.ts
@@ -3,7 +3,8 @@ import { camelize } from '../utils'
 import { Compiler } from './types'
 
 export const JSXCompiler = <Compiler>(async(svg, collection, icon, options) => {
-  const svgr = (await importModule('@svgr/core')).default
+  // @svgr/core mixes default and named exports, which causes an akward extra `default` here
+  const svgr = (await importModule('@svgr/core')).default.default;
   let res = await svgr(svg, {}, { componentName: camelize(`${collection}-${icon}`) })
   // svgr does not provide an option to support preact (WHY?),
   // we manually remove the react import for preact
