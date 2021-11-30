da8ed48d55ba42d85fa888bcf666713b64d278b3
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-11-30 15:00:03 +0100

    chore: change transform callback signature

--- src/loaders.ts
+++ src/loaders.ts
@@ -3,7 +3,7 @@ import { Awaitable } from '@antfu/utils'
 import { camelize, pascalize } from './core/utils'
 import { CustomIconLoader } from '.'
 
-export function FileSystemIconLoader(dir: string, transform?: (svg: string) => Awaitable<string | undefined>): CustomIconLoader {
+export function FileSystemIconLoader(dir: string, transform?: (svg: string) => Awaitable<string>): CustomIconLoader {
   return async(name) => {
     const pathes = [
       `${dir}/${name}.svg`,
