8fdd1df8b54a3b30bccabc463fec9367db0549bf
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-11-30 14:30:27 +0100

    feat: add transform callback to `FileSystemIconLoader`

--- src/loaders.ts
+++ src/loaders.ts
@@ -1,17 +1,21 @@
 import { existsSync, promises as fs } from 'fs'
+import { Awaitable } from '@antfu/utils'
 import { camelize, pascalize } from './core/utils'
 import { CustomIconLoader } from '.'
 
-export function FileSystemIconLoader(dir: string): CustomIconLoader {
-  return (name) => {
+export function FileSystemIconLoader(dir: string, transform?: (svg: string) => Awaitable<string | undefined>): CustomIconLoader {
+  return async(name) => {
     const pathes = [
       `${dir}/${name}.svg`,
       `${dir}/${camelize(name)}.svg`,
       `${dir}/${pascalize(name)}.svg`,
     ]
     for (const path of pathes) {
-      if (existsSync(path))
-        return fs.readFile(path, 'utf-8')
+      if (existsSync(path)) {
+        return typeof transform === 'function'
+          ? transform(await fs.readFile(path, 'utf-8'))
+          : fs.readFile(path, 'utf-8')
+      }
     }
   }
 }
