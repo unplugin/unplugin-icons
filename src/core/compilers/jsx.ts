84500bd14bcab8ab01d02a69840c8d726576948a
Joaquín Sánchez <joaquin.sanchez@fi2net.es>
2021-12-26 05:54:34 +0100

    fix(compiler-jsx): check for multiple svgr version (#138)

--- src/core/compilers/jsx.ts
+++ src/core/compilers/jsx.ts
@@ -3,7 +3,9 @@ import { camelize } from '../utils'
 import { Compiler } from './types'
 
 export const JSXCompiler = <Compiler>(async(svg, collection, icon, options) => {
-  const svgr = (await importModule('@svgr/core')).default
+  const svgrCore = await importModule('@svgr/core')
+  // check for v6 transform, v5 default and previous versions
+  const svgr = svgrCore.transform || svgrCore.default || svgrCore
   let res = await svgr(svg, {}, { componentName: camelize(`${collection}-${icon}`) })
   // svgr does not provide an option to support preact (WHY?),
   // we manually remove the react import for preact
