e12b8ecc1e4de0c1be0320318694fb6f5d3ee5e2
Spark <79936503+spa5k@users.noreply.github.com>
2022-02-07 19:15:13 +0530

    chore(deps): ⬆️ upgrading the dependencies, increasing range for peer dependencies

--- src/core/compilers/jsx.ts
+++ src/core/compilers/jsx.ts
@@ -1,15 +1,24 @@
-import { importModule } from 'local-pkg'
-import { camelize } from '../utils'
-import type { Compiler } from './types'
+import { importModule } from "local-pkg";
+import { camelize } from "../utils";
+import type { Compiler } from "./types";
 
-export const JSXCompiler = <Compiler>(async(svg, collection, icon, options) => {
-  const svgrCore = await importModule('@svgr/core')
+export const JSXCompiler = <Compiler>(async (
+  svg,
+  collection,
+  icon,
+  options
+) => {
+  const svgrCore = await importModule("@svgr/core");
   // check for v6 transform, v5 default and previous versions
-  const svgr = svgrCore.transform || svgrCore.default || svgrCore
-  let res = await svgr(svg, {}, { componentName: camelize(`${collection}-${icon}`) })
+  const svgr = svgrCore.transform || svgrCore.default || svgrCore;
+  let res = await svgr(
+    svg,
+    {},
+    { componentName: camelize(`${collection}-${icon}`) }
+  );
   // svgr does not provide an option to support preact (WHY?),
   // we manually remove the react import for preact
-  if (options.jsx !== 'react')
-    res = res.replace('import * as React from "react";', '')
-  return res
-})
+  if (options.jsx !== "react")
+    res = res.replace('import * as React from "react";', "");
+  return res;
+});
