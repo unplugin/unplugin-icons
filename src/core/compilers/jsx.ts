64a463ee8e29cfacb23cfe3333f45107908656ee
Spark <79936503+spa5k@users.noreply.github.com>
2022-02-08 18:01:16 +0530

    chore(deps): upgrading the dependencies, increasing range for peerdependencies to fix #161 (#162)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- src/core/compilers/jsx.ts
+++ src/core/compilers/jsx.ts
@@ -2,11 +2,20 @@ import { importModule } from 'local-pkg'
 import { camelize } from '../utils'
 import type { Compiler } from './types'
 
-export const JSXCompiler = <Compiler>(async(svg, collection, icon, options) => {
+export const JSXCompiler = <Compiler>(async(
+  svg,
+  collection,
+  icon,
+  options,
+) => {
   const svgrCore = await importModule('@svgr/core')
   // check for v6 transform, v5 default and previous versions
   const svgr = svgrCore.transform || svgrCore.default || svgrCore
-  let res = await svgr(svg, {}, { componentName: camelize(`${collection}-${icon}`) })
+  let res = await svgr(
+    svg,
+    {},
+    { componentName: camelize(`${collection}-${icon}`) },
+  )
   // svgr does not provide an option to support preact (WHY?),
   // we manually remove the react import for preact
   if (options.jsx !== 'react')
--- test/idSvg.test.ts
+++ test/idSvg.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from 'vitest'
+import { expect, test } from 'vitest'
 import { handleSVGId } from '../src/core/svgId'
 
 const fixture = `
