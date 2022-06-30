82963002f67034f3c0b01bad5d844f152ee2a154
Waleed Khaled <waleed1kh@outlook.com>
2022-06-30 12:10:12 +0400

    prebuild generate icon-sets.json for collections array

--- src/core/collections.ts
+++ src/core/collections.ts
@@ -1,5 +1,5 @@
-// TODO: @iconify-json/indexes
-export const collections = ['mdi', 'mdi-light', 'ic', 'ph', 'icon-park-outline', 'icon-park', 'uil', 'bx', 'ri', 'la', 'iconoir', 'jam', 'ion', 'bi', 'clarity', 'carbon', 'cil', 'gg', 'tabler', 'teenyicons', 'fa-solid', 'fa-regular', 'vaadin', 'icomoon-free', 'eva', 'pixelarticons', 'majesticons', 'ci', 'eos-icons', 'dashicons', 'entypo', 'zondicons', 'flat-color-icons', 'octicon', 'codicon', 'ant-design', 'lucide', 'fe', 'radix-icons', 'line-md', 'system-uicons', 'akar-icons', 'uiw', 'uim', 'uit', 'uis', 'gridicons', 'mono-icons', 'heroicons-outline', 'heroicons-solid', 'file-icons', 'mi', 'ps', 'el', 'foundation', 'typcn', 'subway', 'raphael', 'icons8', 'wpf', 'iwwa', 'topcoat', 'ei', 'bytesize', 'fluent', 'grommet-icons', 'pepicons', 'maki', 'oi', 'et', 'vscode-icons', 'fontisto', 'fa', 'zmdi', 'whh', 'si-glyph', 'ls', 'simple-line-icons', 'flat-ui', 'vs', 'websymbol', 'il', 'bpmn', 'fontelico', 'feather', 'noto', 'noto-v1', 'twemoji', 'openmoji', 'emojione', 'emojione-monotone', 'emojione-v1', 'fxemoji', 'logos', 'cib', 'simple-icons', 'fa-brands', 'brandico', 'entypo-social', 'cryptocurrency', 'gis', 'map', 'geo', 'cif', 'fad', 'wi', 'healthicons', 'medical-icon', 'ep']
+import collections from './icon-sets.json'
+export { collections }
 
 export function hasCollection(collection: string) {
   return collections.includes(collection)
