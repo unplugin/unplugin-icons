8f31171e23a3a9258c0acfff673dc43d893910b9
Waleed Khaled <waleed1kh@outlook.com>
2022-06-30 11:42:33 +0400

    Revert "generate collections array from iconify json at compile time"
    This reverts commit b9a43609a0e96bd971392526c893419282fc44a9.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,29 +1,18 @@
 import type { Options } from 'tsup'
-import { promises as fs } from 'fs'
 
 export const tsup: Options = {
   splitting: true,
   clean: true,
   dts: true,
-  entryPoints: ['src/*.ts'],
-  esbuildPlugins: [
-    {
-      name: 'collectionsWriter',
-      async setup(build) {
-        const jc = `export const collections = ${JSON.stringify(Object.keys(
-          JSON.parse(await fs.readFile('node_modules/@iconify/json/collections.json', 'utf8'))
-        ))}`
-
-        build.onLoad({ filter: /core[\/\\]collections\.ts/ }, async ({ path }) => {
-          const code = await fs.readFile(path, 'utf8')
-          return {
-            contents: code.replace(/export const collections = \[.*\]/, jc),
-            loader: 'ts',
-          }
-        })
-      },
-    },
+  entryPoints: [
+    'src/*.ts',
   ],
-  format: ['esm', 'cjs'],
-  external: ['vue', '@iconify/json/package.json'],
+  format: [
+    'esm',
+    'cjs',
+  ],
+  external: [
+    'vue',
+    '@iconify/json/package.json',
+  ],
 }
