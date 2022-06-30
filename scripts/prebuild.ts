5c0328915ee55536964ce2a755dde473d0835ebf
Waleed Khaled <waleed1kh@outlook.com>
2022-06-30 17:20:30 +0400

    fix: pre-build script that generate the collections array from @iconify JSON (#217)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

---
+++ scripts/prebuild.ts
@@ -0,0 +1,9 @@
+import { basename, resolve } from 'path'
+import { promises as fs } from 'fs'
+import jc from '@iconify/json/collections.json'
+
+(async () => {
+  const file = resolve(__dirname, '../src/core/icon-sets.json')
+  console.log('[prebuild]', basename(file))
+  await fs.writeFile(file, JSON.stringify(Object.keys(jc).sort(), null, 2))
+})()
