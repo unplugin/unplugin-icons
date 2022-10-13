9bba49ca3a454d3c3981ed67f6115121b7a6a8cd
userquin <userquin@gmail.com>
2022-10-13 21:52:08 +0200

    fix: update sveltekit example

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -1,20 +1,10 @@
 import preprocess from 'svelte-preprocess'
-import Icons from 'unplugin-icons/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: preprocess(),
-  kit: {
-    vite: {
-      plugins: [
-        Icons({
-          compiler: 'svelte',
-        }),
-      ],
-    },
-  },
 }
 
 export default config
