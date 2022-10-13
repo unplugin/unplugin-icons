2f872fb832e145301b64891168143b09064a64cc
Michael Stramel <mstramel@amazon.com>
2022-10-13 13:44:10 -0500

    docs(examples): add Astro example

---
+++ examples/astro/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import Icons from 'unplugin-icons/vite'
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+      }),
+    ],
+  },
+});
