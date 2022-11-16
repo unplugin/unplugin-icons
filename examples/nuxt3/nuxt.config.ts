01dbf546b656e09df210a71b58b77e49d49d0283
Chris Cabrera <ccabrera@torticity.com>
2022-11-16 16:19:43 -0500

    fix: nuxt 3.0.0 stable support

--- examples/nuxt3/nuxt.config.ts
+++ examples/nuxt3/nuxt.config.ts
@@ -1,5 +1,3 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: [
--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,12 +1,14 @@
 import type { Options } from './types'
 import unplugin from '.'
 
-export default function (this: any, options: Options = {}) {
-  if (this.nuxt?._version?.startsWith('3.'))
+export default function (this: any, options: Options = {}, nuxt: any) {
+  const nuxtApp = this?.nuxt || nuxt
+
+  if (nuxtApp?._version?.startsWith('3.'))
     options.compiler = 'vue3'
 
   // install webpack plugin
-  this.nuxt.hook('webpack:config', (configs: any[]) => {
+  nuxtApp.hook('webpack:config', (configs: any[]) => {
     configs.forEach((config) => {
       config.plugins = config.plugins || []
       config.plugins.unshift(unplugin.webpack(options))
@@ -14,7 +16,7 @@ export default function (this: any, options: Options = {}) {
   })
 
   // install vite plugin
-  this.nuxt.hook('vite:extend', async (vite: any) => {
+  nuxtApp.hook('vite:extend', async (vite: any) => {
     vite.config.plugins = vite.config.plugins || []
     vite.config.plugins.push(unplugin.vite(options))
   })
