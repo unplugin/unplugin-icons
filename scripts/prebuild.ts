a08e87d15644f443b31607d8eb521d1ac974c44d
Anthony Fu <anthonyfu117@hotmail.com>
2022-11-22 18:51:49 +0800

    chore: update deps

--- scripts/prebuild.ts
+++ scripts/prebuild.ts
@@ -5,5 +5,5 @@ import jc from '@iconify/json/collections.json'
 (async () => {
   const file = resolve(__dirname, '../src/core/icon-sets.json')
   console.log('[prebuild]', basename(file))
-  await fs.writeFile(file, JSON.stringify(Object.keys(jc).sort(), null, 2))
+  await fs.writeFile(file, `${JSON.stringify(Object.keys(jc).sort(), null, 2)}\n`)
 })()
