2eb9558ca0feb4399b8c3a59b673b23e3ca7ef7f
river <laolei@forkway.cn>
2023-01-25 17:56:57 +0800

    docs: add qwik example

---
+++ examples/vite-qwik/vite.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from 'vite';
+import {qwikVite} from '@builder.io/qwik/optimizer';
+import {qwikCity} from '@builder.io/qwik-city/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import Icons from 'unplugin-icons/vite';
+
+export default defineConfig(() => {
+    return {
+        plugins: [Icons({
+            autoInstall: true,
+            compiler: 'qwik',
+        }), , qwikCity(), qwikVite(), tsconfigPaths()],
+        preview: {
+            headers: {
+                'Cache-Control': 'public, max-age=600',
+            },
+        },
+    };
+});
