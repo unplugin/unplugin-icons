5668fbb38281f77b31ff79469f8179e3de5e64e4
Nathan Huisman <huis.nathan@gmail.com>
2023-08-28 05:24:16 +0200

    fix(compiler-solid): Props replacement not working with multiline starttags (#301)

--- src/core/compilers/solid.ts
+++ src/core/compilers/solid.ts
@@ -1,6 +1,6 @@
 import type { Compiler } from './types'
 
 export const SolidCompiler = ((svg: string) => {
-  const svgWithProps = svg.replace(/([{}])/g, '{\'$1\'}').replace(/(?<=<svg.*?)(>)/i, '{...props}>')
+  const svgWithProps = svg.replace(/([{}])/g, '{\'$1\'}').replace(/(?<=<svg[\s\S]*?)(>)/i, '{...props}>')
   return `export default (props = {}) => ${svgWithProps}`
 }) as Compiler
