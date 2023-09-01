507706c4cec62d99d89a3bc17cd8bd551d540341
userquin <userquin@gmail.com>
2023-09-01 22:35:28 +0200

    chore: update svelte kit preprocess with vite built-in one

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -1,10 +1,10 @@
-import preprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: vitePreprocess(),
 }
 
 export default config
