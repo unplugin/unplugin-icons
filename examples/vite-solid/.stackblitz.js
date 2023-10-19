866909403467d7a3d55cc2a6e23e84563b48b50a
userquin <userquin@gmail.com>
2023-10-19 20:58:48 +0200

    fix: vite-solid, add dependencies

--- examples/vite-solid/.stackblitz.js
+++ examples/vite-solid/.stackblitz.js
@@ -6,13 +6,7 @@ async function updatePackageJson() {
   const filename = './package.json'
   try {
     const contents = await promises.readFile(filename, 'utf-8')
-    let updatedContent = contents.replace('workspace:*', 'latest')
-    if (!updatedContent.includes('"@svgr/plugin-jsx"')) {
-      updatedContent = updatedContent.replace('"vite-plugin-solid": "^2.7.0"', `"vite-plugin-solid": "^2.7.0",
-    "@svgr/core": ">=7.0.0",
-    "@svgr/plugin-jsx": "^8.1.0"`)
-    }
-
+    const updatedContent = contents.replace('workspace:*', 'latest')
     await promises.writeFile(filename, updatedContent)
   }
   catch (err) {
