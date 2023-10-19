83ddb4469f40d5d39e04ebcd32ddae08bd6fb8bb
userquin <userquin@gmail.com>
2023-10-19 20:26:28 +0200

    chore: cleanup vite-vue2

--- examples/vite-vue2/.stackblitz.js
+++ examples/vite-vue2/.stackblitz.js
@@ -8,10 +8,10 @@ async function updatePackageJson() {
     const contents = await promises.readFile(filename, 'utf-8')
     const updatedContent = contents
       .replace('workspace:*', 'latest')
-      .replace('"vite": "^4.4.9"', `
-    "vite": "^4.4.9",
+      .replace('"vite": "^4.4.9"', `"vite": "^4.4.9",
     "vue-template-compiler": "^2.6.12",
     "vue-template-es2015-compiler": "^1.9.0"`)
+
     await promises.writeFile(filename, updatedContent)
   }
   catch (err) {
