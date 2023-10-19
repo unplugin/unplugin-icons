3708cf64129962e6fbd686d5b8f6e275cca276f8
userquin <userquin@gmail.com>
2023-10-19 20:39:38 +0200

    fix: vite-(p)react, avoid adding deps twice

--- examples/vite-preact/.stackblitz.js
+++ examples/vite-preact/.stackblitz.js
@@ -6,8 +6,7 @@ async function updatePackageJson() {
   const filename = './package.json'
   try {
     const contents = await promises.readFile(filename, 'utf-8')
-    let updatedContent = contents
-      .replace('workspace:*', 'latest')
+    let updatedContent = contents.replace('workspace:*', 'latest')
     if (!updatedContent.includes('"@svgr/plugin-jsx"')) {
       updatedContent = updatedContent.replace('"vite-plugin-inspect": "^0.7.38"', `"vite-plugin-inspect": "^0.7.38",
     "@svgr/plugin-jsx": "^8.1.0"`)
