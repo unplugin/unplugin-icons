7214e01b6ddacc79767c14cec2dab86dc5214b4d
userquin <userquin@gmail.com>
2023-10-19 20:36:24 +0200

    fix: next

--- examples/next/.stackblitz.js
+++ examples/next/.stackblitz.js
@@ -6,7 +6,12 @@ async function updatePackageJson() {
   const filename = './package.json'
   try {
     const contents = await promises.readFile(filename, 'utf-8')
-    const updatedContent = contents.replace('workspace:*', 'latest')
+    let updatedContent = contents.replace('workspace:*', 'latest')
+    if (!updatedContent.includes('"@svgr/plugin-jsx"')) {
+      updatedContent = updatedContent.replace('"unplugin-icons": "latest"', `"unplugin-icons": "latest",
+    "@svgr/plugin-jsx": "^8.1.0"`)
+    }
+
     await promises.writeFile(filename, updatedContent)
   }
   catch (err) {
