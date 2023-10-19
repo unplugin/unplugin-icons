82c9cd3d24703255055c6e0aae10153d3e695299
userquin <userquin@gmail.com>
2023-10-19 20:31:12 +0200

    fix: vite-preact

--- examples/vite-preact/.stackblitz.js
+++ examples/vite-preact/.stackblitz.js
@@ -6,7 +6,11 @@ async function updatePackageJson() {
   const filename = './package.json'
   try {
     const contents = await promises.readFile(filename, 'utf-8')
-    const updatedContent = contents.replace('workspace:*', 'latest')
+    const updatedContent = contents
+      .replace('workspace:*', 'latest')
+      .replace('"vite-plugin-inspect": "^0.7.38"', `"vite-plugin-inspect": "^0.7.38",
+    "@svgr/plugin-jsx": "^8.1.0"`)
+
     await promises.writeFile(filename, updatedContent)
   }
   catch (err) {
