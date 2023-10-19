6f8ef5a5d627d649c79196d7e56a56dd47090462
userquin <userquin@gmail.com>
2023-10-19 19:19:44 +0200

    chore: include all examples (except webpack)

---
+++ examples/vite-vue3/.stackblitz.js
@@ -0,0 +1,17 @@
+import { promises as fsPromises } from 'node:fs'
+
+updatePackageJson()
+
+async function updatePackageJson() {
+  const filename = './package.json'
+  try {
+    const contents = await fsPromises.readFile(filename, 'utf-8')
+    const updatedContent = contents
+      .replace('workspace:*', 'latest')
+      .replace('cross-env DEBUG=unplugin-icons:*', '')
+    await fsPromises.writeFile(filename, updatedContent)
+  }
+  catch (err) {
+    console.error(err)
+  }
+}
