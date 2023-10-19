3c2118a9321bc2682eaead4314153de032652813
userquin <userquin@gmail.com>
2023-10-19 20:26:41 +0200

    fix: vite-solid

--- examples/vite-solid/.stackblitz.js
+++ examples/vite-solid/.stackblitz.js
@@ -6,7 +6,12 @@ async function updatePackageJson() {
   const filename = './package.json'
   try {
     const contents = await promises.readFile(filename, 'utf-8')
-    const updatedContent = contents.replace('workspace:*', 'latest')
+    const updatedContent = contents
+      .replace('workspace:*', 'latest')
+      .replace('"vite-plugin-solid": "^2.7.0"', `"vite-plugin-solid": "^2.7.0",
+    "@svgr/core": ">=7.0.0",
+    "@svgr/plugin-jsx": "^8.1.0"`)
+
     await promises.writeFile(filename, updatedContent)
   }
   catch (err) {
