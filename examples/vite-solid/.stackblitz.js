7fc344e615eae49588ee47ca17edd2aff16080f5
userquin <userquin@gmail.com>
2023-10-19 20:48:45 +0200

    fix: vite-solid, wrong plugin replace

--- examples/vite-solid/.stackblitz.js
+++ examples/vite-solid/.stackblitz.js
@@ -8,7 +8,7 @@ async function updatePackageJson() {
     const contents = await promises.readFile(filename, 'utf-8')
     let updatedContent = contents.replace('workspace:*', 'latest')
     if (!updatedContent.includes('"@svgr/plugin-jsx"')) {
-      updatedContent = updatedContent.replace('"vite-plugin-inspect": "^0.7.38"', `"vite-plugin-inspect": "^0.7.38",
+      updatedContent = updatedContent.replace('"vite-plugin-solid": "^2.7.0"', `"vite-plugin-solid": "^2.7.0",
     "@svgr/core": ">=7.0.0",
     "@svgr/plugin-jsx": "^8.1.0"`)
     }
