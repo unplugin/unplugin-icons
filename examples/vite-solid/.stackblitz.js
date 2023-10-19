bfd364ae2ac43e4fbe4443c70234b6ec5a5d5ca9
userquin <userquin@gmail.com>
2023-10-19 20:40:33 +0200

    fix: vite-solid, avoid adding deps twice

--- examples/vite-solid/.stackblitz.js
+++ examples/vite-solid/.stackblitz.js
@@ -6,11 +6,12 @@ async function updatePackageJson() {
   const filename = './package.json'
   try {
     const contents = await promises.readFile(filename, 'utf-8')
-    const updatedContent = contents
-      .replace('workspace:*', 'latest')
-      .replace('"vite-plugin-solid": "^2.7.0"', `"vite-plugin-solid": "^2.7.0",
+    let updatedContent = contents.replace('workspace:*', 'latest')
+    if (!updatedContent.includes('"@svgr/plugin-jsx"')) {
+      updatedContent = updatedContent.replace('"vite-plugin-inspect": "^0.7.38"', `"vite-plugin-inspect": "^0.7.38",
     "@svgr/core": ">=7.0.0",
     "@svgr/plugin-jsx": "^8.1.0"`)
+    }
 
     await promises.writeFile(filename, updatedContent)
   }
