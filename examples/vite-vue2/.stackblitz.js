10734c0dbdbae5ee26b8cdf43b6f88f621be256b
userquin <userquin@gmail.com>
2023-10-19 20:15:39 +0200

    fix: vite-vue2

--- examples/vite-vue2/.stackblitz.js
+++ examples/vite-vue2/.stackblitz.js
@@ -6,7 +6,12 @@ async function updatePackageJson() {
   const filename = './package.json'
   try {
     const contents = await promises.readFile(filename, 'utf-8')
-    const updatedContent = contents.replace('workspace:*', 'latest')
+    const updatedContent = contents
+      .replace('workspace:*', 'latest')
+      .replace('"vite": "^4.4.9"', `
+    "vite": "^4.4.9",
+    "vue-template-compiler": "^2.6.12",
+    "vue-template-es2015-compiler": "^1.9.0"`)
     await promises.writeFile(filename, updatedContent)
   }
   catch (err) {
