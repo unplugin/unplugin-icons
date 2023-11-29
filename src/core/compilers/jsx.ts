39e4c367b637dd289545fb2fc09617db1a928ce5
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-29 13:36:27 +0100

    chore: update deps

--- src/core/compilers/jsx.ts
+++ src/core/compilers/jsx.ts
@@ -11,8 +11,8 @@ export const JSXCompiler = (async (
   const svgrCore = await importModule('@svgr/core')
   // check for v6/v7 transform (v7 on CJS it is in default), v5 default and previous versions
   const svgr = svgrCore.transform // v6 or v7 ESM
-      || (svgrCore.default ? (svgrCore.default.transform /* v7 CJS */ ?? svgrCore.default) : svgrCore.default)
-      || svgrCore
+    || (svgrCore.default ? (svgrCore.default.transform /* v7 CJS */ ?? svgrCore.default) : svgrCore.default)
+    || svgrCore
   let res = await svgr(
     svg,
     {
