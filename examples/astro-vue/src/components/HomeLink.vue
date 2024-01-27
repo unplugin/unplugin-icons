5154edffe87baef2389b6ef4d523a0b38869d1cc
Jhon Berrio <jhoxsan@gmail.com>
2024-01-27 07:47:58 -0500

    docs: add Astro + Vue example

---
+++ examples/astro-vue/src/components/HomeLink.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import IconParkOutlineHome from '~icons/icon-park-outline/home';
+</script>
+
+<script lang="ts">
+export default {
+  name: 'HomeLink'
+}
+</script>
+
+<template>
+  <a
+    aria-label="Go Home"
+    href="/">
+    <IconParkOutlineHome />
+  </a>
+</template>
