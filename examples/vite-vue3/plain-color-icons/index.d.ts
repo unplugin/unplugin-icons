2510cc588d57473e9662a73b867edc5d072aed54
Anthony Fu <anthonyfu117@hotmail.com>
2024-02-04 11:44:40 +0100

    chore: lint

--- examples/vite-vue3/plain-color-icons/index.d.ts
+++ examples/vite-vue3/plain-color-icons/index.d.ts
@@ -1,13 +1,13 @@
 import type {
-	IconifyJSON,
-	IconifyInfo,
-	IconifyMetaData,
-	IconifyChars,
-} from '@iconify/types';
+  IconifyChars,
+  IconifyInfo,
+  IconifyJSON,
+  IconifyMetaData,
+} from '@iconify/types'
 
-export { IconifyJSON, IconifyInfo, IconifyMetaData, IconifyChars };
+export { IconifyJSON, IconifyInfo, IconifyMetaData, IconifyChars }
 
-export declare const icons: IconifyJSON;
-export declare const info: IconifyInfo;
-export declare const metadata: IconifyMetaData;
-export declare const chars: IconifyChars;
+export declare const icons: IconifyJSON
+export declare const info: IconifyInfo
+export declare const metadata: IconifyMetaData
+export declare const chars: IconifyChars
