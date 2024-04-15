e8078bb8f4703d0815f8477f2175611fcd123693
twt898xu <mcxu233@gmail.com>
2024-04-15 09:16:01 +0800

    fix(): support both old and new versions for local-pkg

--- src/core/compilers/vue2.ts
+++ src/core/compilers/vue2.ts
@@ -11,7 +11,8 @@ export const Vue2Compiler = (async (
   const { compile } = await importModule('vue-template-compiler')
   // eslint-disable-next-line ts/prefer-ts-expect-error
   // @ts-ignore @ts-expect-error
-  const transpile = (await importModule('vue-template-es2015-compiler'))
+  const transpileMod = (await importModule('vue-template-es2015-compiler'))
+  const transpile = transpileMod.default || transpileMod
 
   const { injectScripts, svg: handled } = handleSVGId(svg)
 
