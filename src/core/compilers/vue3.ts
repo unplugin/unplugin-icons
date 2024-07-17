9c5e12cd3973c1bbc2f12c58dcfda8921ec28de9
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2024-07-18 00:53:43 +0530

    feat(vue3): wrap exports with markRaw to allow passing components as props

--- src/core/compilers/vue3.ts
+++ src/core/compilers/vue3.ts
@@ -13,8 +13,11 @@ export const Vue3Compiler = (async (svg: string, collection: string, icon: strin
     filename: `${collection}-${icon}.vue`,
   })
 
+  code = `import { markRaw } from 'vue'\n${code}`
   code = code.replace(/^export /gm, '')
-  code += `\n\nexport default { name: '${collection}-${icon}', render${injectScripts ? `, data() {${injectScripts};return { idMap }}` : ''} }`
+  code += `\n\nexport default markRaw({ name: '${collection}-${icon}', render${
+    injectScripts ? `, data() {${injectScripts};return { idMap }}` : ''
+  } })`
   code += '\n/* vite-plugin-components disabled */'
 
   return code
