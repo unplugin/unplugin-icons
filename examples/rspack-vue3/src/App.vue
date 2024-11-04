dda1ad07bf104eeb64d14944d4e981121c08dff6
carter.luo <carter.luo@oocl.com>
2024-11-04 21:46:00 +0800

    feat: support rspack (#388)
    * feat: support rspack
    
    * chore: add rspack entry in readme file
    
    ---------
    
    Co-authored-by: Carter.Luo <luoca4@oocl.com>

---
+++ examples/rspack-vue3/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import VueLogo from '~icons/logos/vue'
+
+console.log(VueLogo)
+const title = ref("Rspack + Vue");
+</script>
+
+<template>
+	<div>
+		<a href="https://www.rspack.dev/" target="_blank">
+			<img src="./assets/rspack.svg" class="logo" alt="Rspack logo" />
+		</a>
+		<a href="https://vuejs.org/" target="_blank">
+			<VueLogo class="logo vue" style="width: 6em;" alt="Vue logo" />
+		</a>
+	</div>
+	<HelloWorld :msg="title" />
+</template>
+
+<style scoped>
+.logo {
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+	transition: filter 300ms;
+}
+
+.logo:hover {
+	filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+	filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/rspack.ts
@@ -0,0 +1,4 @@
+import type { Options } from './types'
+import unplugin from '.'
+
+export default unplugin.rspack as (options?: Options) => any
