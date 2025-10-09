934fa2a50126356437912e7a0828da4da07abab7
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-10-09 17:56:18 -0400

    Add override to eslint due to an oopsie from array-callback-return

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,15 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default [
+  ...(await antfu()),
+  {
+    name: 'array-callback-return-override',
+    files: ['examples/*-ember/**'],
+    rules: {
+      // This rule assumes that all map() calls are from Array.protoype.
+      // In the ember examples, the Router uses map() to mean "sitemap"
+      'array-callback-return': 'off',
+    },
+  },
+]
