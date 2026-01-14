324f0e764bb146f6f251de653c52022716ed9a81
Kevin Deng <sxzz@sxzz.moe>
2026-01-14 18:47:11 +0800

    chore: simplify prebuild script

--- scripts/prebuild.ts
+++ scripts/prebuild.ts
@@ -1,11 +1,7 @@
-import { promises as fs } from 'node:fs'
+import fs from 'node:fs'
 import { basename, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import jc from '@iconify/json/collections.json'
+import jc from '@iconify/json/collections.json' with { type: 'json' }
 
-(async () => {
-  const __dirname = fileURLToPath(new URL('.', import.meta.url))
-  const file = resolve(__dirname, '../src/core/icon-sets.json')
-  console.log('[prebuild]', basename(file))
-  await fs.writeFile(file, `${JSON.stringify(Object.keys(jc).sort(), null, 2)}\n`)
-})()
+const file = resolve(import.meta.dirname, '../src/core/icon-sets.json')
+console.log('[prebuild]', basename(file))
+fs.writeFileSync(file, `${JSON.stringify(Object.keys(jc).sort(), null, 2)}\n`)
