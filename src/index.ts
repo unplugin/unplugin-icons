49d739f234aa3b409d8f82f2ed66a4cd571f2088
userquin <userquin@gmail.com>
2026-09-05 14:43:48 +0200

    chore: remove meta check when using `addWatchFile` at load hook

--- src/index.ts
+++ src/index.ts
@@ -68,9 +68,7 @@ const unplugin = createUnplugin<Options | undefined>((options = {}, meta) => {
         id,
         config,
         ({ collection, icon }) => {
-          const path = !meta || !(meta.framework === 'vite')
-            ? resolveVirtualIconPath(collection, icon)
-            : undefined
+          const path = resolveVirtualIconPath(collection, icon)
           if (path) {
             this.addWatchFile(path)
           }
